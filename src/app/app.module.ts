f08d3f957c0e8c6a9463e265b3df017b15ba0033
wrm192 <wrm192@gmail.com>
2019-01-20 16:44:00 -0500

    start routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,9 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive'
-import {ShoppingListService} from "./shopping-list/shopping-list.service";
+import { DropdownDirective } from './shared/dropdown.directive';
+import {ShoppingListService} from './shopping-list/shopping-list.service';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import {ShoppingListService} from "./shopping-list/shopping-list.service";
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
     //HttpModule
   ],
   providers: [ShoppingListService],
